def624af64f168bace45d15cd8134147a85687aa
szabobence96 <sz.bence996@gmail.com>
2023-08-26 22:47:59 +0200

    Commit

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -10,11 +10,11 @@ export class BodyComponent {
   @Input() collapsed = false;
   @Input() screenWidth = 0;
 
-  getBodyClass():string{
+  getBodyClass(): string {
     let styleClass = '';
-    if(this.collapsed && this.screenWidth > 768){
+    if (this.collapsed && this.screenWidth > 768) {
       styleClass = 'body-trimmed';
-    } else if(this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0){
+    } else if (this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
       styleClass = 'body-md-screen'
     }
     return styleClass;
